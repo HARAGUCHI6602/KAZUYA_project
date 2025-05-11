e3939a04c7424eddb3cd7c5f11704022435a7a7c
HARAGUCHI KAZUYA <haraguchi1882@gmail.com>
2025-05-11 09:45:29 +0900

    Q03提出2

--- src/basic/q03/SumLoop.java
+++ src/basic/q03/SumLoop.java
@@ -1,10 +1,11 @@
 package basic.q03;
 
 public class SumLoop {
-
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
-	}
-
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
 }
