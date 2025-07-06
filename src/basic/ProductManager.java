c2cedd6b829df3890df6d4f8ea1359ea97bac9e6
HARAGUCHI KAZUYA <haraguchi1882@gmail.com>
2025-07-06 13:57:16 +0900

    課題6週目　提出

--- src/basic/ProductManager.java
+++ src/basic/ProductManager.java
@@ -3,28 +3,53 @@ package basic;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ProductManager implements Searchable {
+public class ProductManager {
+    private List<Product> productList = new ArrayList<>();
 
-    private List<Product> products;
-
-    public ProductManager() {
-        products = new ArrayList<>();
-    }
-
-    // 商品を追加するメソッド
+    // 商品を追加
     public void addProduct(Product product) {
-        products.add(product);
+        productList.add(product);
+        System.out.println(product + " を登録しました。");
     }
 
-    // キーワードで商品名を検索する
-    @Override
-    public List<Product> search(String keyword) {
+    // 商品名で完全一致で1件取得
+    public Product getProductByName(String name) {
+        for (Product p : productList) {
+            if (p.getName().equals(name)) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    // 商品名で部分一致検索（Searchableで対応）
+    public List<Product> searchProducts(String keyword) {
         List<Product> result = new ArrayList<>();
-        for (Product product : products) {
-            if (product.getName().contains(keyword)) {
-                result.add(product);
+        for (Product p : productList) {
+            if (p instanceof Searchable) {
+                if (((Searchable) p).containsName(keyword)) {
+                    result.add(p);
+                }
             }
         }
         return result;
+    }
+
+    // 商品IDで削除
+    public boolean deleteProductById(int id) {
+        for (Product p : productList) {
+            if (p.id == id) {
+                productList.remove(p);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 商品すべてを表示
+    public void showAllProducts() {
+        for (Product p : productList) {
+            System.out.println(p);
+        }
     }
 }
